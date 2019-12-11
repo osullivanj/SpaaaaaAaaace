957cd96837e8e095d1b4d4ba1b95b01529729758
Jamison OSullivan <58435937+osullivanj@users.noreply.github.com>
2019-12-11 00:01:24 -0500

    planet textures

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
 var mercury;
+var venus;
+var earth;
+var mars;
+var jupiter;
+var saturn;
+var uranus;
+var neptune;
+var pluto;
 
 function preload () {
-  mercury = loadImage("mercury.jpg"); 
+  mercury = loadImage("mercury.jpg");
+  venus = loadImage("venus.jpg");
+  earth = loadImage("earth.jpg");
+  mars = loadImage("mars.jpg");
+  jupiter = loadImage("jupiter.jpg");
+  saturn = loadImage("saturn.jpg");
+  uranus = loadImage("uranus.jpg");
+  neptune = loadImage("neptune.jpg");
+  pluto = loadImage("pluto.jpg");
 }
 
 function setup () {
@@ -14,7 +30,7 @@ function draw() {
   background(220);
   // background(0);
   push();
-  // rotateY(frameCount / 2);
+  rotateY(frameCount / 2);
   translate(0, 0, 0);
 
   // Sun
@@ -22,7 +38,6 @@ function draw() {
   sphere(25);
 
   // Mercury
-  
   push();
   rotateY(frameCount / 2);
   translate(40, 0, 0);
@@ -38,11 +53,12 @@ function draw() {
 
   // Venus
   push();
-  rotateY(frameCount / 1);
+  rotateY(frameCount / 4);
   translate(33, 0, 0);
   rotateY(frameCount * 4);
-  rotateX(frameCount * 6);
+  // rotateX(frameCount * 6);
   fill(255, 153, 51);
+  texture(venus);
   sphere(3);
   translate(22, 0, 0);
   fill(255, 153, 51);
@@ -53,8 +69,9 @@ function draw() {
   translate(35, 0, 0);
   push();
   rotateY(frameCount * 4);
-  rotateX(frameCount * 6);
+  // rotateX(frameCount * 6);
   fill("blue");
+  texture(earth);
   sphere(5);
   translate(18, 0, 0);
   fill("blue");
@@ -64,72 +81,78 @@ function draw() {
   // Mars
   translate(40, 0, 0);
   push();
-  rotateY(frameCount * 4)
-  rotateX(frameCount * 6)
+  rotateY(frameCount * 4);
+  // rotateX(frameCount * 6);
   fill("red");
+  texture(mars);
   sphere(7);
   translate(20, 0, 0);
-  fill("red")
+  fill("red");
   sphere(3)
   pop();
 
   // Jupiter
   translate(43, 0, 0);
   push();
-  rotateY(frameCount * 4)
-  rotateX(frameCount * 6)
-  fill(51, 25, 0)
+  rotateY(frameCount * 4);
+  // rotateX(frameCount * 6);
+  fill(51, 25, 0);
+  texture(jupiter);
   sphere(7);
   translate(22, 0, 0);
-  fill(51, 25, 0)
+  fill(51, 25, 0);
   sphere(3)
   pop();
 
   // Saturn
   translate(45, 0, 0);
   push();
-  rotateY(frameCount * 4)
-  rotateX(frameCount * 6)
-  fill(153, 153, 0)
+  rotateY(frameCount * 4);
+  // rotateX(frameCount * 6);
+  fill(153, 153, 0);
+  texture(saturn);
   sphere(10);
   translate(23, 0, 0);
-  fill(153, 153, 0)
+  fill(153, 153, 0);
   sphere(5);
   pop();
 
   // Uranus
   translate(50, 0, 0);
   push();
-  rotateY(frameCount * 4)
-  rotateX(frameCount * 6)
-  fill(0, 255, 255)
+  rotateY(frameCount * 4);
+  // rotateX(frameCount * 6);
+  fill(0, 255, 255);
+  texture(uranus);
   sphere(12);
   translate(25, 0, 0);
-  fill(0, 255, 255)
+  fill(0, 255, 255);
   sphere(6);
   pop();
 
   // Neptune
   translate(55, 0, 0);
   push();
-  rotateY(frameCount * 4)
-  rotateX(frameCount * 6)
-  fill(102, 102, 255)
+  rotateY(frameCount * 4);
+  // rotateX(frameCount * 6);
+  fill(102, 102, 255);
+  texture(neptune);
   sphere(12);
   translate(28, 0, 0);
-  fill(102, 102, 255)
+  fill(102, 102, 255);
   sphere(6);
   pop();
 
   // Pluto
   translate(100, 0, 0);
   push();
-  rotateY(frameCount * 4)
-  rotateX(frameCount * 4)
-  fill(160, 160, 160)
+  rotateY(frameCount * 4);
+  // rotateX(frameCount * 4);
+  fill(160, 160, 160);
+  texture(pluto);
   sphere(4);
   translate(28, 0, 0);
-  fill(160, 160, 160)
+  fill(160, 160, 160);
   sphere(2);
   pop();
 }
